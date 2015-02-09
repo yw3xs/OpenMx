541db8c369ba3e7a540028f0a434c4a18b5f4e3a
Yang Wang <yw3xs@virginia.edu>
2015-02-08 21:56:06 -0500

    fix missing fc->grad

--- src/computeSD.cpp
+++ src/computeSD.cpp
@@ -2,6 +2,27 @@
 
 #include "computeSD.h"
 
+void SD_grad(GradientOptimizerContext &rf)
+{
+    ComputeFit("steep_fd", rf.fitMatrix, FF_COMPUTE_FIT, rf.fc);
+
+    const double refFit = rf.fc->fit;
+    const double eps = 1e-5;
+    Eigen::VectorXd p1(rf.fc->numParam), p2(rf.fc->numParam), grad(rf.fc->numParam);
+
+    memcpy(p1.data(), rf.fc->est, (rf.fc->numParam) * sizeof(double));
+
+    for (int px = 0; px < rf.fc->numParam; px++) {
+        memcpy(p2.data(), rf.fc->est, (rf.fc->numParam) * sizeof(double));
+        p2[px] += eps;
+        memcpy(rf.fc->est, p2.data(), (rf.fc->numParam) * sizeof(double));
+        ComputeFit("steep_fd", rf.fitMatrix, FF_COMPUTE_FIT, rf.fc);
+        grad[px] = (rf.fc->fit - refFit) / eps;
+        memcpy(rf.fc->est, p1.data(), (rf.fc->numParam) * sizeof(double));
+    }
+    rf.fc->grad = grad;
+}
+
 bool FitCompare(GradientOptimizerContext &rf, double speed)
 {
     Eigen::VectorXd prevEst(rf.fc->numParam), currEst(rf.fc->numParam);
@@ -11,7 +32,8 @@ bool FitCompare(GradientOptimizerContext &rf, double speed)
     ComputeFit("steep", rf.fitMatrix, FF_COMPUTE_FIT, rf.fc);
     refFit = rf.fc->fit;
 
-    Eigen::VectorXd searchDir(rf.fc->grad);
+    SD_grad(rf);
+    Eigen::VectorXd searchDir = rf.fc->grad;
     currEst = prevEst - speed / searchDir.norm() * searchDir;
     memcpy(rf.fc->est, currEst.data(), (rf.fc->numParam) * sizeof(double));
     ComputeFit("steep", rf.fitMatrix, FF_COMPUTE_FIT, rf.fc);
@@ -22,25 +44,7 @@ bool FitCompare(GradientOptimizerContext &rf, double speed)
     return newFit < refFit;
 }
 
-void SD_grad(GradientOptimizerContext &rf)
-{
-    ComputeFit("steep_fd", rf.fitMatrix, FF_COMPUTE_FIT, rf.fc);
 
-    const double refFit = rf.fc->fit;
-    const double eps = 1e-5;
-    Eigen::VectorXd p1(rf.fc->numParam), p2(rf.fc->numParam);
-
-    memcpy(p1.data(), rf.fc->est, (rf.fc->numParam) * sizeof(double));
-
-    for (int px = 0; px < rf.fc->numParam; px++) {
-        memcpy(p2.data(), rf.fc->est, (rf.fc->numParam) * sizeof(double));
-        p2[px] += eps;
-        memcpy(rf.fc->est, p2.data(), (rf.fc->numParam) * sizeof(double));
-        ComputeFit("steep_fd", rf.fitMatrix, FF_COMPUTE_FIT, rf.fc);
-        rf.fc->grad[px] = (rf.fc->fit - refFit) / eps;
-        memcpy(rf.fc->est, p1.data(), (rf.fc->numParam) * sizeof(double));
-    }
-}
 
 void steepDES(GradientOptimizerContext &rf, int maxIter)
 {
